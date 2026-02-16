1936d239308a98c471cefffa42d694369d007151
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-16 15:53:55 +0530

    feat : not found page added.

---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>
+        unfortunately, we could not find requested resource or page, please try
+        again later.
+      </p>
+    </main>
+  );
+};
+
+export default NotPage;
