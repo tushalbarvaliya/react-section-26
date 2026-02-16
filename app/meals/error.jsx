b32a19487668e4126a18f171f88294e54aed8494
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-16 15:49:01 +0530

    feat : add meals error page.

---
+++ app/meals/error.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const error = () => {
+  return (
+    <main className="error">
+      <h1>An Error Occurred! </h1>
+      <p>Failed to load meals data. Please Try again later.</p>
+    </main>
+  );
+};
+
+export default error;
