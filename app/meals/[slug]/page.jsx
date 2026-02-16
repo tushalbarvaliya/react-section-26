3f339bd6c24ec4cc0a0e77902f7318987f5694de
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-16 16:16:31 +0530

    feat : meal details page add and fetch the data

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import style from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
 const page = ({ params }) => {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g,'<br />')
   return (
     <>
+      <header className={style.header}>
+        <div className={style.image}>
+          <Image fill src={meal.image} alt={meal.title}/>
+        </div>
+        <div className={style.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={style.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={style.summary}>{meal.summary}</p>
+        </div>
+      </header>
       <main>
-        <h1 style={{ color: "white", textAlign: "center" }}>{params.slug}</h1>
+        <p
+          className={style.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
       </main>
     </>
   );
