f134d7aa77a2fe5161aee7b2c22dc41e4b3ad37d
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-17 09:49:21 +0530

    feat : add form validation.

--- lib/actions.jsx
+++ lib/actions.jsx
@@ -3,18 +3,33 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() == "";
+}
 
+export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
     image: formData.get("image"),
     instructions: formData.get("instructions"),
-    summary: formData.get("summary"),
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid Input");
+  }
   // console.log(meal);
-  await saveMeal(meal)
-  redirect('/meals')
+  await saveMeal(meal);
+  redirect("/meals");
 }
